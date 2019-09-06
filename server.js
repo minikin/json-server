edc73b23474e66297d17caaf18ba15c519777659
minikin <djminikin@gmail.com>
2019-09-06 10:56:41 +0200

    Add db

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
